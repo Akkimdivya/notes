f74c3d7c171a05a23e5ccce5b5b867286c0fdc81
Akkimdivya <divyaakkim3@gmail.com>
2024-01-06 20:38:08 +0530

    hello

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
 const express = require('express')
-const path = require('path')
+const app=express();
 
-const {open} = require('sqlite')
-const sqlite3 = require('sqlite3')
-const cors =require('cors');
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`listening ${process.env.PORT||3000}`);
+});
 
-const dbPath = path.join(__dirname, 'notes.db')
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-
-let db = null
-
-const initializeDBAndServer = async () => {
-  try {
-    db = await open({
-      filename: dbPath,
-      driver: sqlite3.Database,
-    })
-    app.listen(3000, () => {
-      console.log('Server Running at http://localhost:3000/')
-    })
-  } catch (e) {
-    console.log(`DB Error: ${e.message}`)
-    process.exit(1)
-  }
-}
-initializeDBAndServer()
-
-app.get('/notes/', async (request, response) => {
-    const getNotesQuery = `
-      SELECT
-        *
-      FROM
-        list;`
-    const notesArray = await db.all(getNotesQuery)
-    response.send(notesArray)
-  })
+app.get('/',async(req,res)=>{
+    res.send("<h1>hey divi</h1>")
+})
